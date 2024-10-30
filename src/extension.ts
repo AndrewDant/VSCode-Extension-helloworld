29a22edb1f87356c94f2444742200d3ba85bdd4a
Andrew Dant <andrew.dant@clario.com>
2024-10-30 09:56:07 -0400

    small practice changes

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('helloworld.practiceCommand', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
+		vscode.window.showInformationMessage('Test reloading');
+	});
+	const secondDisposable = vscode.commands.registerCommand('helloworld.secondCommand', () => {
+		vscode.window.showWarningMessage(new Date().toTimeString());
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(secondDisposable);
 }
 
 // This method is called when your extension is deactivated
